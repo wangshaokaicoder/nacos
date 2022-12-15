b085cc41dd47455d070358d95495c96b9e9d8af1
wangshaokai <8212781+wangshaokai@user.noreply.gitee.com>
2022-12-15 23:14:09 +0800

    1

--- console/src/main/java/com/alibaba/nacos/Nacos.java
+++ console/src/main/java/com/alibaba/nacos/Nacos.java
@@ -22,15 +22,16 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
- * Nacos starter.
+ * 单机启动的参数  -Dnacos.standalone=true
  *
+ * Nacos starter.
  * @author nacos
  */
 @SpringBootApplication(scanBasePackages = "com.alibaba.nacos")
 @ServletComponentScan
 @EnableScheduling
 public class Nacos {
-    
+
     public static void main(String[] args) {
         SpringApplication.run(Nacos.class, args);
     }
